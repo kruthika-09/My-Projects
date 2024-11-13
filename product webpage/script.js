05b22444967c8d3e7e44f40c22edda78fd43101c
kruthika <kruthikakurapati@gmail.com>
2024-11-13 18:26:58 +0530

    changes made

--- product webpage/script.js
+++ product webpage/script.js
@@ -1,5 +1,6 @@
+//thumbnails function
 function change_image(image) {
-  var container = document.getElementById("main-image");
+  var container = document.querySelector(".main-image");
   container.src = image.src;
 }
 document.addEventListener("DOMContentLoaded", function (event) {});
@@ -8,38 +9,23 @@ function dispalyText() {
   var text = document.getElementById("textField");
   text.style.display = "block";
 }
-const btn4Text = document.getElementById("btn4-text");
-const btn4 = document.getElementById("btn4");
-
-btn4.addEventListener("click", () => {
-  btn4Text.classList.remove("hidden");
-  btn4Text.classList.add("popup-message");
-});
-
-btn4Text.addEventListener("animationend", () => {
-  btn4Text.classList.remove("popup-message");
-  btn4Text.classList.add("hidden");
-});
-
+//quantity function
 let data = 1;
+document.querySelector(".counting").innerText = data;
 
-//printing default value of data that is 0 in h2 tag
-
-document.getElementById("counting").innerText = data;
-
-//creation of increment function
-
+//quantity increment function
 function increment() {
   data = data + 1;
 
-  document.getElementById("counting").innerText = data;
+  document.querySelector(".counting").innerText = data;
 }
-//creation of decrement function
-
+//quantity decrement function
 function decrement() {
   if (data > 0) {
     data = data - 1;
-
-    document.getElementById("counting").innerText = data;
+    document.querySelector(".counting").innerText = data;
   }
 }
+document.querySelector(".cartButton").addEventListener("click", function () {
+  document.querySelector(".popupText").classList.remove("hidden");
+});
